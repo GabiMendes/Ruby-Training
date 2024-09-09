0ecd798ee60e65b0b6d8b1274e4d0a4107145231
Gabriela Mendes <gabi06marquesmd@gmail.com>
2024-09-09 19:22:28 -0300

    8kyu

--- 8kyu-RemoveExclamationMarks.rb
+++ 8kyu-RemoveExclamationMarks.rb
@@ -1,9 +1,5 @@
 # Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 def remove_exclamation_marks(s)
-    result = ''
-    s.each_char do |char|
-      result << char unless char == '!'
-    end
-    result
+    s.gsub('!', '')
   end
