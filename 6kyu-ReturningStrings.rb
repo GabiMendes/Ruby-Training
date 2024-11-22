db828ede57952450e01057ab10d681e00dd76062
Gabriela Mendes <gabi06marquesmd@gmail.com>
2024-11-21 21:22:20 -0300

    6kyu

---
+++ 6kyu-ReturningStrings.rb
@@ -0,0 +1,13 @@
+# Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+def greet(name)
+    "Hello, #{name} how are you doing today?"
+  end
+  
+# Test:
+# describe "greet" do
+#     it "Fixed tests" do
+#       Test.assert_equals(greet("Ryan"), "Hello, Ryan how are you doing today?")
+#       Test.assert_equals(greet("Shingles"), "Hello, Shingles how are you doing today?")
+#     end
+#   end
